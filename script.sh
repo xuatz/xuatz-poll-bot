aaccd936570311bcbd26f1174d89fd980a62e184
xuatz <xzlow10@gmail.com>
2017-07-31 12:07:38 +0800

    update script

--- script.sh
+++ script.sh
@@ -3,4 +3,4 @@ git pull;
 docker build -t xuatz/xuatz-poll-bot . ;
 docker stop xuatz-poll-bot ;
 docker rm xuatz-poll-bot ;
-docker run -d --name=xuatz-poll-bot xuatz/xuatz-poll-bot ;
+docker run -d --name=xuatz-poll-bot --restart=always xuatz/xuatz-poll-bot ;
