e2ec87c49428a451ceeb96920eeb83bec1316dc6
Low Xuan Zhi <xuanzhi.low@fusionsystems.co.jp>
2018-05-21 18:31:27 +0900

    included dummy server to comply with now's requirement

--- server.js
+++ server.js
@@ -2,161 +2,332 @@ if (!process.env.NODE_ENV) {
     require("dotenv").config();
 }
 
-const Telegraf = require("telegraf");
+const TeleBot = require("telebot");
 const _ = require("lodash");
+const loki = require("lokijs");
+const http = require("http");
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.use(Telegraf.memorySession());
+const bot = new TeleBot({
+    token: process.env.BOT_TOKEN,
+    usePlugins: ["askUser"]
+});
 
-// console.log(Telegraf);
+// On start command
+bot.on("/start", msg => {
+    // const id = msg.from.id;
+    // // Ask user name
+    // return bot.sendMessage(id, "What is your name?", { ask: "name" });
+});
 
-let hasPoll = false;
-
-let state = {
-    options: ["dummyOption1", "dummyOption2"],
-    votes: [
-        {
-            user: "dummyUser1",
-            option: "dummyOption1"
-        },
-        {
-            user: "dummyUser2",
-            option: "dummyOption1"
-        },
-        {
-            user: "dummyUser3",
-            option: "dummyOption2"
-        }
-    ]
+//db code will be moved to db.js, quite soon
+let States;
+const databaseInitialize = () => {
+    States = db.getCollection("states");
+    if (States === null) {
+        States = db.addCollection("states");
+    }
 };
 
-bot.command("start", ({ from, reply }) => {
-    console.log("start", from);
-    return reply("Welcome!");
-});
+const db = new loki("xpb.db");
+databaseInitialize();
 
-bot.hears("hi", ctx => ctx.reply("Hey there!"));
-bot.on("sticker", ctx => ctx.reply("👍"));
+const getData = msg => {
+    const offset = msg.entities[0].length;
+    const data = msg.text.slice(offset + 1);
+    return data;
+};
 
-bot.command("/oldschool", ctx => ctx.reply("Hello"));
+const initialState = {
+    userList: {},
+    name: "",
+    options: [],
+    votes: []
+};
 
-bot.command("/poll", ctx => {
-    if (!hasPoll) {
-        ctx.reply("Okay, new poll started!");
-        hasPoll = true;
+const getStateForChat = chatID => {
+    let currentState = States.findOne({
+        chatID
+    });
+
+    if (!currentState) {
+        currentState = Object.assign({}, _.cloneDeep(initialState), {
+            chatID
+        });
+    }
+
+    return currentState;
+};
+
+// =============================================
+// === cleaned start
+// =============================================
+
+bot.on("/new", msg => {
+    // console.log(msg);
+    // console.log(msg.chat.id);
+
+    let state = getStateForChat(msg.chat.id);
+    let name = getData(msg);
+
+    if (name && name.length > 0) {
+        if (!state.meta) {
+            States.insert(
+                Object.assign({}, state, {
+                    name
+                })
+            );
+        } else {
+            States.update(
+                Object.assign({}, state, _.cloneDeep(initialState), {
+                    name
+                })
+            );
+        }
+
+        msg.reply.text("Got it. Okay guys start voting!");
     } else {
-        ctx.reply("There is already a poll");
+        msg.reply.text("Starting a new poll!").then(res => {
+            if (!state.meta) {
+                States.insert(state);
+            } else {
+                state = Object.assign({}, state, _.cloneDeep(initialState));
+                state.userList[msg.from.id] = "title";
+                States.update(state);
+            }
+            return bot.sendMessage(msg.chat.id, "What is the poll about?", {
+                ask: "title",
+                replyMarkup: {
+                    force_reply: true,
+                    selective: true
+                },
+                replyToMessage: msg.message_id
+            });
+        });
     }
 });
 
-bot.command("/addoption", ctx => {
-    let option = ctx.update.message.text.slice(11);
-    console.log(option.length, option);
+bot.on("*", (msg, props) => {
+    let state = getStateForChat(msg.chat.id);
+
+    const userID = msg.from.id;
+    const ask = state.userList[userID];
+
+    if (ask) {
+        bot.event("ask." + ask, msg, props);
+        state.userList[userID] = false;
+        States.update(state);
+    }
+});
+
+bot.on("/titlechange", msg => {
+    let state = getStateForChat(msg.chat.id);
+
+    state.userList[msg.from.id] = "titlechange";
+    States.update(state);
+
+    return bot.sendMessage(msg.chat.id, "What is the title of the poll?", {
+        ask: "titlechange",
+        replyMarkup: {
+            force_reply: true,
+            selective: true
+        },
+        replyToMessage: msg.message_id
+    });
+});
+
+// Ask name event
+bot.on("ask.title", msg => {
+    let state = getStateForChat(msg.chat.id);
+
+    //TODO should include empty check
+    state.name = msg.text;
+    States.update(state);
+
+    msg.reply.text(`Understood.\nOkay guys start voting on:\n${msg.text}`);
+});
+
+bot.on("ask.titlechange", msg => {
+    let state = getStateForChat(msg.chat.id);
+
+    //TODO should include empty check
+    state.name = msg.text;
+    States.update(state);
+
+    msg.reply.text(`Okay! Title of the poll changed to:\n${msg.text}`);
+});
+
+// =============================================
+// === cleaned end
+// =============================================
+
+// Ask name event
+
+bot.on("/add", msg => {
+    let state = getStateForChat(msg.chat.id);
+
+    let option = getData(msg).trim();
 
     if (option && option.length > 0) {
-        state.options.push(option);
+        const responses = [
+            "not literally!",
+            "very funny",
+            "roflcopter i am flying",
+            "Somebody's gonna get hurt, real bad"
+        ];
 
-        ctx.reply(option + " added as an option").then(result => {
-            console.log(result);
-            ctx.reply("current options are: " + state.options);
-        });
+        const helperMsg = `\ntype \`/add <actual option>\` e.g. \`/add pikachu\``;
 
-        console.log(state.options);
+        switch (option) {
+            case "nothing":
+                return bot.sendMessage(
+                    msg.chat.id,
+                    "You *literally* added nothing!" + helperMsg,
+                    { parseMode: "Markdown", replyToMessage: msg.message_id }
+                );
+            case "something":
+            case "<something>":
+            case "option":
+            case "<option>":
+                return bot.sendMessage(
+                    msg.chat.id,
+                    responses[Math.round(Math.random() * 3)] + helperMsg,
+                    { parseMode: "Markdown", replyToMessage: msg.message_id }
+                );
+            case "<actual option>":
+            case "actual option":
+                return bot.sendMessage(msg.chat.id, "RKO OUTTA NOWHERE!!!", {
+                    parseMode: "Markdown",
+                    replyToMessage: msg.message_id
+                });
+            default: {
+                state.options.push(option);
+                States.update(state);
+                return msg.reply.text(option + " added as an option");
+            }
+        }
+    } else {
+        bot.sendMessage(
+            msg.chat.id,
+            "You added nothing! type `/add <something>` to add option",
+            { parseMode: "Markdown", replyToMessage: msg.message_id }
+        );
     }
 });
 
-bot.command("/vote", ctx => {
-    console.log("/vote =======================");
-    let { first_name, username } = ctx.update.message.from;
+bot.on("/vote", msg => {
+    let state = getStateForChat(msg.chat.id);
 
-    let user = username || first_name;
-    let option = ctx.update.message.text.slice(6);
+    let option = getData(msg);
+    if (option && option.length > 0) {
+        bot
+            .sendMessage(msg.chat.id, "Your vote is captured", {
+                replyMarkup: {
+                    remove_keyboard: true,
+                    selective: true
+                },
+                replyToMessage: msg.message_id
+            })
+            .then(res => {
+                bot.deleteMessage(res.result.chat.id, res.result.message_id);
 
-    console.log(option.length, option);
-    console.log(ctx);
-    console.log(ctx.update.message);
+                // CASE INSENSITIVE CHECK -- START
+                // Create a regex based on 'option', to search case insensitively
+                let searchregex = new RegExp(`^${option}$`, "i");
 
-    // ctx.reply(
-    //     "One time keyboard",
-    //     Telegraf.Markup
-    //         .keyboard(["/simple", "/inline", "/pyramid"])
-    //         .oneTime()
-    //         .resize()
-    //         .extra()
-    // );
-
-    if (state.options.length > 0) {
-        if (option && option.length > 0) {
-            let index = state.votes.findIndex(vote => {
-                return vote.user == user;
-            });
-
-            let vote = {
-                user,
-                option
-            };
-
-            if (index !== -1) {
-                state.votes[index] = vote;
-            } else {
-                state.votes.push(vote);
-                // console.log("=====", state.votes);
-            }
-
-            // console.log(ctx);
-            // console.log(ctx.update);
-
-            ctx.reply(user + " have voted for: " + option).then(res => {
-                let tmp = _.groupBy(state.votes, vote => vote.option);
-                // console.log(tmp);
-                let tmp2 = [];
-                let tmp3 = "";
-                _.forIn(tmp, (value, key) => {
-                    tmp2.push({
-                        option: key,
-                        count: value.length
-                    });
-                    tmp3 += key + ": " + value.length + "\n";
+                // Search each vote using the regex. matchedIndex is assigned -1 if no votes match case insensitive search
+                let matchedIndex = state.votes.findIndex(vote => {
+                    return vote.option.match(searchregex) !== null;
                 });
 
-                console.log(tmp3);
+                // For matched result, make the current vote option follow previously existing variant
+                if (matchedIndex !== -1) {
+                    option = state.votes[matchedIndex].option;
+                }
+                // CASE INSENSITIVE CHECK -- END
 
-                ctx.reply(`current votes: \n=======================\n${tmp3}`);
+                let vote = {
+                    user: msg.from,
+                    option
+                };
+                let index = state.votes.findIndex(vote => {
+                    if (vote.user) {
+                        return vote.user.id == msg.from.id;
+                    }
+                });
+                if (index !== -1) {
+                    state.votes[index] = vote;
+                } else {
+                    state.votes.push(vote);
+                }
+
+                States.update(state);
+            })
+            .then(() => {
+                bot.event("/result", msg);
+            })
+            .catch(err => {
+                console.log(err);
             });
-        } else {
-            ctx.reply(
-                "random example",
-                Telegraf.Markup
-                    .keyboard(
-                        state.options.map(option => {
-                            return "/vote " + option;
-                        })
-                    )
-                    .oneTime()
-                    .resize()
-                    .extra()
+    } else {
+        if (state.options.length == 0) {
+            return bot.sendMessage(
+                msg.chat.id,
+                `There is no official options added yet!\nTry \`/add <option>\` or /vote <option> directly!`,
+                { parseMode: "Markdown", replyToMessage: msg.message_id }
             );
+        } else {
+            let replyMarkup = bot.keyboard(
+                state.options.map(option => {
+                    return [bot.button("/vote " + option)];
+                })
+            );
+
+            // console.log(msg);
+            // msg.from.id
+            // msg.message_id
+            // msg.chat.id
+            return bot.sendMessage(msg.chat.id, "Please cast your vote", {
+                replyMarkup: Object.assign({}, replyMarkup, {
+                    resize_keyboard: true,
+                    selective: true
+                }),
+                replyToMessage: msg.message_id
+            });
         }
     }
 });
 
-bot.on("callback_query", ctx => {
-    console.log("callback???");
+bot.on("/result", msg => {
+    let state = getStateForChat(msg.chat.id);
 
-    console.log(ctx);
-    console.log(ctx.tg);
-    console.log(ctx.update);
+    let tmp = _.groupBy(state.votes, vote => vote.option);
+    let tmp3 = "";
+    _.forIn(tmp, (value, key) => {
+        tmp3 += `${key} (${value.length} votes) \n`;
 
-    ctx.getChat().then(res => {
-        console.log("=========");
-        console.log(res);
+        // if (Math.random() > 0.5) {
+        //     tmp3 += value.length + ": " + key + "\n";
+        // } else {
+
+        // }
+
+        value.forEach(vote => {
+            tmp3 += "- " + (vote.user.first_name || vote.user.username) + "\n";
+        });
     });
+    let text = `topic: ${state.name}\n`;
+    text += `=======================\n`;
+    text += `${tmp3}`;
 
-    // ctx.deleteMessage();
-    ctx.deleteMessage();
+    msg.reply.text(text);
 });
 
-console.log(Telegraf);
-// console.log(Telegraf.Telegram.toString());
+bot.start();
 
-bot.startPolling();
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello World!\n");
+});
+
+server.listen(3000);
